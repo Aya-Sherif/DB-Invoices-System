d55b9a13365d3ee33744b21295ffb59814c2c52a
Aya-Sherif <118867207+Aya-Sherif@users.noreply.github.com>
2023-05-06 23:30:49 +0300

    1130

---
+++ invoices system/invoices system/Pages/Accounting_invoices_Data.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace invoices_system.Pages
+{
+    public class Accounting_invoices_DataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
