ff54893598a9173c26bac5dfbbf102fb70629626
Aya-Sherif <118867207+Aya-Sherif@users.noreply.github.com>
2023-05-11 21:17:17 +0300

    Accounting department

---
+++ invoices system/invoices system/Pages/AccountantMain.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace invoices_system.Pages
+{
+    public class AccountantMainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
