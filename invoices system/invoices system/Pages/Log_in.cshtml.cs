be6a2f7f15ab19f0bb4e1bc461177aaa930ad401
Mahmoudelshahed7623 <117799034+Mahmoudelshahed7623@users.noreply.github.com>
2023-05-19 11:15:06 +0300

    finish log in

--- invoices system/invoices system/Pages/Log_in.cshtml.cs
+++ invoices system/invoices system/Pages/Log_in.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using invoices_system.Models;
 using System.Data;
-
 namespace invoices_system.Pages
 {
     public class Log_inModel : PageModel
@@ -24,26 +23,23 @@ namespace invoices_system.Pages
         {
             if (db.CheckPassword(W))
             {
-                if (db.GetWorkerRoleID(W) == "1")
+                if (db.GetWorkerRoleID(W) == "SE")
                 {
-                    return Page();
+                    return RedirectToPage("/SiteEngineerInvoices");
                 }
-                else if(db.GetWorkerRoleID(W) == "1")
-                {
-                    return RedirectToPage("/");
-                }
-                else if (db.GetWorkerRoleID(W) == "1")
+                else if(db.GetWorkerRoleID(W) == "SM")
                 {
                     return RedirectToPage("/");
                 }
-                else if (db.GetWorkerRoleID(W) == "1")
+                else if (db.GetWorkerRoleID(W) == "PM")
                 {
-                    return RedirectToPage("/");
+                    return RedirectToPage("/ProjectManagerMain");
                 }
-                else if (db.GetWorkerRoleID(W) == "1")
+                else if (db.GetWorkerRoleID(W) == "AC")
                 {
-                    return RedirectToPage("/");
+                    return RedirectToPage("/AccountantMain");
                 }
+                
                 else { return Page(); }
             }
             else { return Page(); }
