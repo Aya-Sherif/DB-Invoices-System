a24fd811c7cb9ba644eeff375687291961b84e0c
Mahmoudelshahed7623 <117799034+Mahmoudelshahed7623@users.noreply.github.com>
2023-05-20 09:14:57 +0300

    edit func in DB class

--- invoices system/invoices system/Models/DB.cs
+++ invoices system/invoices system/Models/DB.cs
@@ -128,24 +128,24 @@ namespace invoices_system.Models
             string Q = "  select invoiceID,invoiceType,startDate,invoiceStatus,contractorName,endDate from Invoice where projectID='"+PID+"'                      ";
             return FuncExecuteReadr(Q);
         }
-        public DataTable getS_Project_AC(string PID)//PM
+        public string getS_Project_AC(string PID)//PM
         {
 
             string Q = "  select workerName from Worker w where w.workerID=(select w.workerID from Works_On wo , Worker w where wo.workerID=w.workerID  and w.roleID='AC' and wo.projectID='"+PID+"')                     ";
-            return FuncExecuteReadr(Q);
-        }
+            return FuncExecuteStringScaler(Q);
+		}
 
-        public DataTable getS_Project_PM(string PID)//PM
+        public string getS_Project_PM(string PID)//PM
         {
 
             string Q = "  select workerName from Worker w where w.workerID=(select w.workerID from Works_On wo , Worker w where wo.workerID=w.workerID  and w.roleID='PM' and wo.projectID='" + PID + "')                     ";
-            return FuncExecuteReadr(Q);
+            return FuncExecuteStringScaler(Q);
         }
-        public DataTable getS_Project_SE(string PID)//PM
+        public string getS_Project_SE(string PID)//PM
         {
 
             string Q = "  select workerName from Worker w where w.workerID=(select w.workerID from Works_On wo , Worker w where wo.workerID=w.workerID  and w.roleID='SE' and wo.projectID='" + PID + "')                     ";
-            return FuncExecuteReadr(Q);
+            return FuncExecuteStringScaler(Q);
         }
     }
 }
